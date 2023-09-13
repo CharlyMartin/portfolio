1b7c9317fb1813d50548862cb1a1e18dbaac4de7
Charly Martin <charly@hey.com>
2023-09-13 13:13:48 +0100

    Switch from marked to remark

--- src/data/articles/best-ui-react-component.md
+++
@@ -1,1 +0,0 @@
-<!-- https://dev.to/blossom/8-best-practices-for-reactjs-component-design-4jn5 -->
--- src/lib/parse-markdown.ts
+++ src/lib/parse-markdown.ts
@@ -1,32 +1,21 @@
 // https://www.npmjs.com/package/gray-matter
-// https://github.com/markedjs/marked/issues/655
 
 import { readFileSync } from "fs";
 import { join } from "path";
-import { marked } from "marked";
 import matter from "gray-matter";
+import { unified } from "unified";
+import rehypeExternalLinks from "rehype-external-links";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import { visit } from "unist-util-visit";
 
 import { addUtm } from "./add-utm";
 
-const renderer = new marked.Renderer();
-
-renderer.link = function addTargetToLinks(href, title, text) {
-  if (!href) return "";
-
-  const t = title || text;
-
-  if (href.startsWith("/")) {
-    return `<a href="${href}" title="${t}">${text}</a>`;
-  }
-
-  return `<a href="${addUtm(
-    href
-  )}" target="_blank" title="${t}" rel="noopener noreferrer">${text}</a>`;
-};
-
 const root = process.cwd();
 
-type Location = "projects" | "bio";
+type Location = "projects" | "bio" | "articles";
 
 type Return = {
   metadata: Record<string, string>;
@@ -41,12 +30,32 @@ export function parseFileContent(location: Location, slug: string): Return {
 
   const { data, content } = matter(source);
 
+  const processor = unified()
+    .use(remarkParse as any)
+    .use(remarkGfm as any)
+    .use(remarkRehype as any)
+    .use(rehypeExternalLinks, {
+      rel: ["noopener", "noreferrer"],
+      target: "_blank",
+    })
+    .use(addUtmPlugin)
+    .use(rehypeStringify);
+
+  const file = processor.processSync(content);
+
   return {
     metadata: data,
-    html: marked(content, {
-      mangle: false,
-      headerIds: false,
-      renderer,
-    }),
+    html: String(file),
+  };
+}
+
+function addUtmPlugin() {
+  return function plugin(tree: any) {
+    visit(tree, "element", function updateUrl(node) {
+      // Only add utm info to external links
+      if (node.tagName == "a" && !node.properties.href.startsWith("/")) {
+        node.properties.href = addUtm(node.properties.href);
+      }
+    });
   };
 }
